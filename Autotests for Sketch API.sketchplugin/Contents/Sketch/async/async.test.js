4764cb3d89d0a249db2f16982edbc7325b45cdd1
Sergey Golyshev <serjiosgol@gmail.com>
2020-06-30 19:42:10 +0500

    add async and setting tests

---
+++ Autotests for Sketch API.sketchplugin/Contents/Sketch/async/async.test.js
@@ -0,0 +1,38 @@
+var test = require('__autoTest').test;
+var expect = require('__autoTest').expect;
+
+var createFiber = require('sketch/async').createFiber;
+
+test('should create a fiber', () => {
+  const fiber = createFiber()
+  fiber.cleanup()
+  expect(fiber).toBeDefined()
+  expect(fiber.cleanup).toBeDefined()
+  fiber.cleanup()
+  expect(fiber.onCleanup).toBeDefined()
+})
+
+test('onCleanup should be called when cleaning up the fiber', () => {
+  const fiber = createFiber()
+  let cleanedUp = false
+  fiber.onCleanup(() => {
+    cleanedUp = true
+  })
+  expect(cleanedUp).toBe(false)
+  fiber.cleanup()
+  expect(cleanedUp).toBe(true)
+})
+
+test('should keep the plugin around when using a fiber', () => {
+  //expect.assertions(1)
+  // creates a fiber
+  return new Promise(resolve => {
+    coscript.scheduleWithInterval_jsFunction(
+      0.1, // 0.1s
+      () => {
+        expect(true).toBe(true)
+        resolve()
+      }
+    )
+  })
+})
